3eb4ff8ea9474d37fd27b8e5ff2ff0042e496da8
thisistia99 <32358554+thisistia99@users.noreply.github.com>
2018-08-01 20:11:45 +0530

    Create life_universe.c

---
+++ life_universe.c
@@ -0,0 +1,17 @@
+//solution to the problem 'life,universe and everything'
+
+#include<stdio.h>
+int main()
+{
+	int n;
+	while(1){
+		scanf("%d", &n);
+		if(n!=42)
+			printf("%d ", n);
+		if(n==42)
+			break;
+				
+	}
+		
+return 0;	
+}
